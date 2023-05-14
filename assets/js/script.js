9e55428518a2e230956b3bc869a10dbe56ebd214
Job Karani <jobkarani084j@gmail.com>
2023-05-14 12:19:35 +0300

    feat: contact page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,69 +91,6 @@ document.addEventListener('DOMContentLoaded',function(event){
   StartTextAnimation(0);
 });
 
-// slider 
-window.onload=function(){
-  (function(d){
-   var
-   ce=function(e,n){var a=document.createEvent("CustomEvent");a.initCustomEvent(n,true,true,e.target);e.target.dispatchEvent(a);a=null;return false},
-   nm=true,sp={x:0,y:0},ep={x:0,y:0},
-   touch={
-    touchstart:function(e){sp={x:e.touches[0].pageX,y:e.touches[0].pageY}},
-    touchmove:function(e){nm=false;ep={x:e.touches[0].pageX,y:e.touches[0].pageY}},
-    touchend:function(e){if(nm){ce(e,'fc')}else{var x=ep.x-sp.x,xr=Math.abs(x),y=ep.y-sp.y,yr=Math.abs(y);if(Math.max(xr,yr)>20){ce(e,(xr>yr?(x<0?'swl':'swr'):(y<0?'swu':'swd')))}};nm=true},
-    touchcancel:function(e){nm=false}
-   };
-   for(var a in touch){d.addEventListener(a,touch[a],false);}
-  })(document);
-  var h=function(e){console.log(e.type,e)};
-  document.body.addEventListener('fc',h,false);
-      document.getElementById('slider-cont').addEventListener('swr',mySlideRight,false);
-  document.getElementById('slider-cont').addEventListener('swl',mySlideLeft,false);
-  }
-  function mySlideRight(){
-          var sli=document.getElementsByClassName('cont-slider');
-      for(var i=0;i<sli.length;i++){
-          if(sli[i].id=="ext-1"){sli[i].id='ext-8'}else{
-          var eid=sli[i].id;
-          var strnum = eid.match(/\d+$/)[0];
-          var num=parseInt(strnum);
-              strnum--;
-          sli[i].id='ext-'+strnum;
-          }
-      }
-      
-  }
-  function mySlideLeft(){
-      var sli=document.getElementsByClassName('cont-slider');
-      for(var i=0;i<sli.length;i++){
-          if(sli[i].id=="ext-8"){sli[i].id='ext-1'}else{
-          var eid=sli[i].id;
-          var strnum = eid.match(/\d+$/)[0];
-          var num=parseInt(strnum);
-              strnum++;
-          sli[i].id='ext-'+strnum;
-          }
-      }
-  }
-  function selectSliderElement(e){
-      var idd=e.id;
-      var numstr = idd.match(/\d+$/)[0];
-      var numid=parseInt(numstr);
-      var sli=document.getElementsByClassName('cont-slider');
-      if(numid==1){}else{
-          var times=9-numid;
-          for (var z=0;z<times;z++)
-          for(var i=0;i<sli.length;i++){
-          if(sli[i].id=="ext-8"){sli[i].id='ext-1'}else{
-          var eid=sli[i].id;
-          var strnum = eid.match(/\d+$/)[0];
-          var num=parseInt(strnum);
-              strnum++;
-          sli[i].id='ext-'+strnum;
-          }
-      }}
-      }
-
 // testimonials 
 'use strict'
 var	testim = document.getElementById("testim"),
@@ -262,22 +199,22 @@ window.onload = function() {
 		})
 }
 
-// navbar on scroll 
-// $(document).ready(function() {
-//   var previousScroll = 0;
-//   $(window).scroll(function() {
-//     var currentScroll = $(this).scrollTop();
+// contact form 
 
-//     if (currentScroll < previousScroll) {
-//       // Scrolling up
-//       $('#navbar1').css('top', '0');
-//       $('#navbar2').css('top', '-50px');
-//     } else {
-//       // Scrolling down
-//       $('#navbar1').css('top', '-50px');
-//       $('#navbar2').css('top', '0');
-//     }
+function whatsapp(){
+  var FullName = document.getElementById("fullname").value;
+  var Email = document.getElementById("email").value;
+  var Phone = document.getElementById("phone").value;
+  var Subject = document.getElementById("subject").value;
+  var Message = document.getElementById("message").value;
 
-//     previousScroll = currentScroll;
-//   });
-// });
+  var url = "https://wa.me/254795398841?text="
+  +"* FullName : * " + FullName + "%0a"
+  +"* Email : * " + Email + "%0a"
+  +"* Phone : * " + Phone + "%0a"
+  +"* Subject : * " + Subject + "%0a"
+  +"* Message : * " + Message + "%0a"
+
+  window.open(url, '_blank').focus;
+
+}
