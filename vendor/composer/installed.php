ece7fa6d9360b601d9891cdaa0ff78d72042aeb3
gwatai <gwatainicholas@gmail.com>
2024-04-28 17:06:59 +0300

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chebu/mykura-website',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e1cef2bdafe3386bb10e0203acf1a9e04a0d8725',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chebu/mykura-website' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e1cef2bdafe3386bb10e0203acf1a9e04a0d8725',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
